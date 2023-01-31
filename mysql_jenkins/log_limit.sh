28566f8bf0ce414991357160b1f1851e83bb08ca
maxim <maxrockvardill@gmail.com>
2023-01-31 18:58:29 +0300

    update_jenkinsfile

--- mysql_jenkins/log_limit.sh
+++ mysql_jenkins/log_limit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam -e \'select * from clan limit 10;\' > log_limit.txt
+mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam -e 'select * from clan limit 10;' > log_limit.txt
